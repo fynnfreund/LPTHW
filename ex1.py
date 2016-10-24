d0b5ef729b8ca266a393cb46ef8c77d18487a9e8
fynnfreund <fyfriend@live.de>
2016-10-24 15:19:32 +0200

    Add files via upload

---
+++ ex1.py
@@ -0,0 +1,11 @@
+#print "Hello World!"
+#print "Hello Again"
+#print "I like typing this."
+#print "This is fun."
+#print 'Yay! Printing.'
+#print "I'd much rather you 'not'."
+print "I 'said' do not touch this."
+# print "Ja was denn"
+
+# this is a comment and will be ignored by python. 
+print "this is not a comment and will be printed by python"
---
+++ ex11.py
@@ -0,0 +1,8 @@
+print "How old are you?",
+age = raw_input()
+print "How tall are you? (in cm)",
+height = raw_input()
+print "How much much do you weigh? (in kg)",
+weight = raw_input()
+
+print "So, you're %r old, %r cm tall and %r kg heavy." % (age, height, weight)
